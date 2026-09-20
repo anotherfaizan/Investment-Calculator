2f0ba38d87c7cd8f1ab272d5ff006f78920a72b9
anotherfaizan <mdfaizankhan2002@gmail.com>
2026-09-20 20:32:52 +0530

    Investment Ouptut

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
+
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
-import { type InvestmentInput } from './investment-input.model';
+import type { InvestmentReturns, InvestmentInput } from './investment-input.model';
+import { InvestmentResultsComponent } from './investment-results/investment-results.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
+  returnInvestmentData?: InvestmentReturns[];
 
   calculateInvestmentResults(data: InvestmentInput) {
     const { initialInvestment, annualInvestment, expectedReturn, duration } = data;
@@ -32,8 +35,7 @@ export class AppComponent {
       });
     }
 
-    console.log(annualData);
-    // return annualData;
+    this.returnInvestmentData = annualData;
   }
 
 
